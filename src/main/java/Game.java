9cdbf47cde99f08479772171854fe9a16689f743
Dora Charm <kovina.dora@gmail.com>
2023-06-12 14:35:52 +0500

    Task 1

---
+++ src/main/java/Game.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+public class Game {
+
+    ArrayList<Player> players = new ArrayList<>();
+
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+    public void register(Player player) {
+
+        for (int i = 0; i < players.size(); i++) {
+            Player currentPlayer = players.get(i);
+            if (currentPlayer.getId() == player.getId()) {
+                throw new AlreadyExistsException(
+                        "Игрок с id " + player.getId() + " уже существует."
+                );
+            }
+            if (currentPlayer.getName() == player.getName()) {
+                throw new AlreadyExistsException(
+                        "Игрок с именем " + player.getName() + " уже существует."
+                );
+            }
+        }
+
+        players.add(player);
+    }
+
+    public int round (String playerName1, String playerName2) {
+
+        Player player1 = findByName(playerName1);
+        Player player2 = findByName(playerName2);
+        int player1Strength = player1.getStrength();
+        int player2Strength = player2.getStrength();
+
+        if (player1Strength > player2Strength) {
+            return 1;
+        } else if (player1Strength < player2Strength) {
+            return 2;
+        } else {
+            return 0;
+        }
+    }
+
+    public Player findByName(String name) {
+        for (Player player :
+             players) {
+            if (player.getName() == name) {
+                return player;
+            }
+        }
+        throw new NotRegisteredException(
+                "Игрок с именем " + name + " не зарегистрирован."
+        );
+    }
+}
---
+++ src/test/java/GameTest.java
@@ -0,0 +1,112 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    Game game = new Game();
+
+    Player player1 = new Player(1, "Monster", 2);
+    Player player2 = new Player(2, "Huge", 1);
+    Player player3 = new Player(3, "Vanya", 1);
+
+    @BeforeEach
+    public void setup() {
+        game.register(player1);
+        game.register(player2);
+        game.register(player3);
+    }
+
+    @Test
+    public void register() {
+
+        ArrayList<Player> list = new ArrayList<>();
+        list.add(player1);
+        list.add(player2);
+        list.add(player3);
+
+        boolean expected = true;
+        boolean actual = list.equals(game.getPlayers());
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotAddPlayerWithExistingId() {
+
+        Player player4 = new Player(2, "Nikita", 3);
+
+        Assertions.assertThrows(AlreadyExistsException.class, () -> {
+            game.register(player4);
+        });
+    }
+
+    @Test
+    public void shouldNotAddPlayerWithExistingName() {
+
+        Player player4 = new Player(4, "Vanya", 3);
+
+        Assertions.assertThrows(AlreadyExistsException.class, () -> {
+            game.register(player4);
+        });
+    }
+
+    @Test
+    public void shouldFindRegisteredPlayerByName() {
+
+        Player expected = player1;
+        Player actual = game.findByName("Monster");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldThrowNotRegisteredException() {
+
+        Player player4 = new Player(4, "Kesha", 3);
+
+        Assertions.assertThrows(NotRegisteredException.class, () -> {
+            game.findByName("Kesha");
+        });
+    }
+
+    @Test
+    public void firstPlayerShouldWin() {
+
+        int expected = 1;
+        int actual = game.round("Monster", "Huge");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void secondPlayerShouldWin() {
+
+        int expected = 2;
+        int actual = game.round("Vanya", "Monster");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldTie() {
+
+        int expected = 0;
+        int actual = game.round("Vanya", "Huge");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldThrowNotRegisteredException2() {
+
+        Player player4 = new Player(4, "Denys", 3);
+
+        Assertions.assertThrows(NotRegisteredException.class, () ->
+                game.round("Denys", "Huge"));
+    }
+}
